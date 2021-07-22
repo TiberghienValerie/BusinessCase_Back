8d8b91739b0eaece363a919afe858e117dbba9b4
TiberghienValerie <tiberghien.valerie@wanadoo.fr>
2021-07-22 11:02:23 +0200

    Ajout nom, prenom entity user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -66,6 +66,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $garages;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
     public function __construct()
     {
         $this->garages = new ArrayCollection();
@@ -235,6 +245,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
                 $garage->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
 
         return $this;
     }
