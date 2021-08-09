d38f55ff815cdf16cc53fb963eca7e893f7c163b
TiberghienValerie <tiberghien.valerie@wanadoo.fr>
2021-08-09 16:57:32 +0200

    MAJ des controllers pour garage

--- src/Controller/GarageController.php
+++ src/Controller/GarageController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Garage;
 use App\Entity\Ville;
+use App\Repository\GarageRepository;
 use App\Repository\UserRepository;
+use App\Repository\VilleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,15 +22,23 @@ class GarageController extends AbstractController
 
     private UserRepository $userRepository;
 
+    private VilleRepository $villeRepository;
+
+    private GarageRepository $garageRepository;
+
     /**
      * GarageController constructor.
      * @param EntityManagerInterface $em
      * @param UserRepository $userRepository
+     * @param VilleRepository $villeRepository
+     * @param GarageRepository $garageRepository
      */
-    public function __construct(EntityManagerInterface $em, UserRepository $userRepository)
+    public function __construct(EntityManagerInterface $em, UserRepository $userRepository, VilleRepository $villeRepository, GarageRepository $garageRepository)
     {
         $this->em = $em;
         $this->userRepository = $userRepository;
+        $this->villeRepository = $villeRepository;
+        $this->garageRepository = $garageRepository;
     }
 
 
@@ -36,18 +46,29 @@ class GarageController extends AbstractController
      * @Route("/garage/add", name="garage")
      * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_PROFESSIONNEL')")
      */
-    public function index(Request $request): Response
+    public function add(Request $request): Response
     {
         $post_data = json_decode($request->getContent());
 
 
         $user = $this->userRepository->find($post_data->user);
-        $ville = new Ville();
-        $ville->setNomVille($post_data->nomVille);
-        $ville->setCodePostal($post_data->codePostal);
-        $ville->setAdresse1($post_data->adresse1);
-        $ville->setAdresse2($post_data->adresse2);
-        $ville->setAdresse3($post_data->adresse3);
+
+        $ville = $this->villeRepository->findOneBy([
+            'nomVille'=>$post_data->nomVille,
+            'codePostal' => $post_data->codePostal,
+            'adresse1' => $post_data->adresse1,
+            'adresse2' => $post_data->adresse2,
+            'adresse3' => $post_data->adresse3
+        ]);
+
+        if($ville === null) {
+            $ville = new Ville();
+            $ville->setNomVille($post_data->nomVille);
+            $ville->setCodePostal($post_data->codePostal);
+            $ville->setAdresse1($post_data->adresse1);
+            $ville->setAdresse2($post_data->adresse2);
+            $ville->setAdresse3($post_data->adresse3);
+        }
         $garage = new Garage();
         $garage->setNom($post_data->nom);
         $garage->setTelephone($post_data->telephone);
@@ -57,6 +78,43 @@ class GarageController extends AbstractController
         $this->em->persist($ville);
         $this->em->flush();
 
+        return new JsonResponse([
+            'response' => 'bon'
+        ]);
+    }
+
+    /**
+     * @Route("/garage/update/{id}", name="garage")
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_PROFESSIONNEL')")
+     */
+    public function update(Request $request, string $id): Response
+    {
+        $post_data = json_decode($request->getContent());
+
+        $ville = $this->villeRepository->findOneBy([
+            'nomVille'=>$post_data->nomVille,
+            'codePostal' => $post_data->codePostal,
+            'adresse1' => $post_data->adresse1,
+            'adresse2' => $post_data->adresse2,
+            'adresse3' => $post_data->adresse3
+        ]);
+
+        if($ville === null) {
+            $ville = new Ville();
+            $ville->setNomVille($post_data->nomVille);
+            $ville->setCodePostal($post_data->codePostal);
+            $ville->setAdresse1($post_data->adresse1);
+            $ville->setAdresse2($post_data->adresse2);
+            $ville->setAdresse3($post_data->adresse3);
+        }
+
+        $garage = $this->garageRepository->find($id);
+        $garage->setNom($post_data->nom);
+        $garage->setTelephone($post_data->telephone);
+        $garage->setVille($ville);
+        $this->em->persist($garage);
+        $this->em->persist($ville);
+        $this->em->flush();
 
         return new JsonResponse([
             'response' => 'bon'
--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -34,6 +34,7 @@ class UserDataPersister implements DataPersisterInterface
 
     public function supports($data): bool
     {
+
         return $data instanceof User;
     }
 
